06dd780e020890599921024de5c026aee8bf19ab
Cristóvão Olegário <cristovaoolegario@gmail.com>
2021-08-05 10:38:23 -0300

    Adding Category model to project.

---
+++ models/category.go
@@ -0,0 +1,11 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Category struct {
+	ID     primitive.ObjectID `bson:"_id" json:"id"`
+	Titulo string             `bson:"titulo" json:"titulo"`
+	Cor    string             `bson:"cor" json:"cor"`
+}

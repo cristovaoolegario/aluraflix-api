80453a2d5e490a3fb94b94d21dd5b9c8308624bf
Cristóvão Olegário <cristovaoolegario@gmail.com>
2021-11-25 01:01:41 -0300

    Refactor insert_category test

--- internal/pkg/http/dto/insert_category_test.go
+++ internal/pkg/http/dto/insert_category_test.go
@@ -1,9 +1,10 @@
 package dto
 
 import (
+	"testing"
+
 	"github.com/stretchr/testify/assert"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-	"testing"
 )
 
 func TestInsertCategory_ConvertToCategory(t *testing.T) {
@@ -19,32 +20,34 @@ func TestInsertCategory_ConvertToCategory(t *testing.T) {
 	assert.IsType(t, primitive.ObjectID{}, category.ID)
 }
 
-func TestInsertVideo_Validate_ShouldReturnError_WhenTituloIsEmpty(t *testing.T) {
-	ic := InsertCategory{
-		Titulo: "",
-		Cor:    "Red",
-	}
-	err := ic.Validate()
+func TestInsertCategory_Validate(t *testing.T) {
+	t.Run("Should return error when titulo is empty", func(t *testing.T) {
+		ic := InsertCategory{
+			Titulo: "",
+			Cor:    "Red",
+		}
+		err := ic.Validate()
 
-	assert.Equal(t, "Titulo is required.", err.Error())
-}
+		assert.Equal(t, "Titulo is required.", err.Error())
+	})
 
-func TestInsertVideo_Validate_ShouldReturnError_WhenCorIsEmpty(t *testing.T) {
-	ic := InsertCategory{
-		Titulo: "Unit Test Title",
-		Cor:    "",
-	}
-	err := ic.Validate()
+	t.Run("Should return error when cor is empty", func(t *testing.T) {
+		ic := InsertCategory{
+			Titulo: "Unit Test Title",
+			Cor:    "",
+		}
+		err := ic.Validate()
 
-	assert.Equal(t, "Cor is required.", err.Error())
-}
+		assert.Equal(t, "Cor is required.", err.Error())
+	})
 
-func TestInsertVideo_Validate_ShouldNotReturnError_WhenInsertCategoryIsValid(t *testing.T) {
-	ic := InsertCategory{
-		Titulo: "Unit Test Title",
-		Cor:    "Red",
-	}
-	err := ic.Validate()
+	t.Run("Should not return error when insert category is valid", func(t *testing.T) {
+		ic := InsertCategory{
+			Titulo: "Unit Test Title",
+			Cor:    "Red",
+		}
+		err := ic.Validate()
 
-	assert.Nil(t, err)
+		assert.Nil(t, err)
+	})
 }
